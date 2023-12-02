222bdcd229f91f6e9a02c782035bda1607cfa8b3
Daniel Gutierrez <danigutierritos2817@gmail.com>
2023-12-02 17:17:06 -0500

    tailwind listo

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
