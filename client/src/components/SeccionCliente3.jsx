73064d7c2fd93ad9b78588da2507664e498244f9
Daniel Gutierrez <danigutierritos2817@gmail.com>
2023-12-03 12:02:13 -0500

    ultima seccion

--- client/src/components/SeccionCliente3.jsx
+++ client/src/components/SeccionCliente3.jsx
@@ -1,24 +1,57 @@
-
+import planeta from '../../images/planeta.png';
 const SeccionCliente3 = () => {
+
+    const estilotitulo = {
+        color: '#053126',
+        textalign: 'center',
+        fontfamily: 'Rakkas',
+        fontWeight: 'bold',
+    };
+
+    const estilobotones2 = {
+        color: '#CBE9D2',
+        textAlign: 'center',
+        fontFamily: 'Rakkas',
+        fontWeight: 'bold',
+      };
+
     return (
         <>
             <div style={{ display: 'flex', height: 'calc(80vh - 64px)', alignItems: 'center' }}>
                 {/* Columna izquierda con textos */}
-                <div className="flex flex-col mr-8">
-                    <div className="mb-2">
-                        <p className="text-lg">Texto 1</p>
-                        <p className="text-lg">Texto 2</p>
+                <div className="flex flex-col mr-8 ml-8 mt-24">
+                    <div className="mb-12">
+                        <div className="flex">
+                            <p className="text-4xl" style={estilotitulo}>Acumulado:</p>
+                            <p className="text-4xl ml-2 border border-black rounded-xl px-24 py-2" style={estilotitulo}>Texto 2</p>
+                        </div>
                     </div>
-                    <div className="mb-2">
-                        <p className="text-lg">Texto 3</p>
-                        <p className="text-lg">Texto 4</p>
+                    <div className="mb-12">
+                        <div className="flex">
+                            <p className="text-4xl" style={estilotitulo}>Porcentaje:</p>
+                            <p className="text-4xl ml-2 border border-black rounded-xl px-24 py-2" style={estilotitulo}>Texto 2</p>
+                        </div>
+                    </div>
+                    <div className="mb-12">
+                        <div className="flex">
+                            <p className="text-4xl" style={estilotitulo}>Recompensa Actual:</p>
+                            <p className="text-4xl ml-2 border border-black rounded-xl px-24 py-2" style={estilotitulo}>Texto 2</p>
+                        </div>
+                    </div>
+                    <div className="mb-12">
+                        <div className="flex">
+                            <p className="text-4xl" style={estilotitulo}>Total:</p>
+                            <p className="text-4xl ml-2 border border-black rounded-xl px-24 py-2" style={estilotitulo}>Texto 2</p>
+                        </div>
                     </div>
                 </div>
 
                 {/* Columna derecha con imagen y botón */}
-                <div>
-                    <img src="tu_imagen.jpg" alt="Imagen" className="mb-4" />
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded">Botón</button>
+                <div className="flex ml-60">
+                    <div className="flex flex-col items-center">
+                        <img src={planeta} alt="Imagen" className="mb-4" style={{ width: '45vh', height: 'auto', }}/>
+                        <button className="bg-blackgreen text-white px-24 py-2 rounded-full border border-blackgreen" style={estilobotones2}>Botón</button>
+                    </div>
                 </div>
 
             </div>
