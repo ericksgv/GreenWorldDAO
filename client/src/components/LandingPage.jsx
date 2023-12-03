28b6b6662b993b1e289ab2a9074d76c9b1f31004
Daniel Gutierrez <danigutierritos2817@gmail.com>
2023-12-02 19:27:35 -0500

    cards

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -14,8 +14,8 @@ const LandingPage = () => {
         textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
         fontFamily: 'Rakkas',
         fontWeight: 'bold',
-      };
-      
+    };
+
 
     return (
         <>
@@ -45,9 +45,22 @@ const LandingPage = () => {
                 </div>
             </div>
             <div className="flex items-center justify-center w-screen">
-                <p className= "text-3xl" style={estiloparrafo}>Siente la Magia de la Naturaleza con Green World</p>
+                <p className="text-3xl" style={estiloparrafo}>Siente la Magia de la Naturaleza con Green World</p>
             </div>
 
+            <div className="flex justify-center">
+                <div className="bg-lightgreen rounded-lg overflow-hidden shadow-md m-4 p-6 w-60 justify-center items-center flex">
+                    <h2 className="text-xl font-bold mb-4 text-center">Genera ingresos mientras adoptas un estilo de vida ecoamigable</h2>
+                </div>
+
+                <div className="bg-lightgreen rounded-lg overflow-hidden shadow-md m-4 p-6 w-60 justify-center items-center flex">
+                    <h2 className="text-xl font-bold mb-4 text-center">Monetiza tu compromiso con lo sostenible</h2>
+                </div>
+
+                <div className="bg-lightgreen rounded-lg overflow-hidden shadow-md m-4 p-6 w-60 justify-center items-center flex">
+                    <h2 className="text-xl font-bold mb-4 text-center">Productos que cuidan, beneficios que perduran</h2>
+                </div>
+            </div>
         </>
     )
 }
