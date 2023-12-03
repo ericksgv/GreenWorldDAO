6f631063e2a1ee176538590bb5f2d1bedb08a4e4
Daniel Gutierrez <danigutierritos2817@gmail.com>
2023-12-02 22:52:55 -0500

    nav cuarta

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import { NavSecundaria } from './components/NavSecundaria.jsx'
 import { NavTercearia } from './components/NavTerceario.jsx'
 import { SeccionCliente2 } from './components/SeccionCliente2.jsx'
+import { NavCuarta } from './components/NavCuarta.jsx'
 const App = () => {
   return (
     <Router>
@@ -24,6 +25,12 @@ const App = () => {
           <Route path="/producto" element={
             <div className="min-h-screen bg-lightgreen">
               <NavTercearia />
+              <SeccionCliente2 />
+            </div>
+          } />
+          <Route path="/contrato" element={
+            <div className="min-h-screen bg-lightgreen">
+              <NavCuarta />
               <SeccionCliente2 />
             </div>
           } />
